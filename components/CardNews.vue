7675442cf6b59c259f0eb9d7d92ca9aa1c8187e2
muhammad <sabirse500@gmail.com>
2025-08-02 18:12:51 +0500

    Refactor CardNews component to use NuxtLink for navigation
    - Replaced anchor tags with NuxtLink components for internal navigation, improving routing efficiency and maintaining SPA behavior.
    - Updated links for tags and card actions to utilize NuxtLink, ensuring consistent navigation experience across the CardNews component.

--- components/CardNews.vue
+++ components/CardNews.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cardNews wow fadeInUp">
-    <a :href="link">
+    <NuxtLink :to="link">
       <div class="cardImage">
         <div class="datePost">
           <div class="heading-52-medium color-white">{{ date }}</div>
@@ -8,20 +8,20 @@
         </div>
         <img :src="img" alt="luxride" />
       </div>
-    </a>
+    </NuxtLink>
     <div class="cardInfo">
       <div class="tags mb-10">
-        <a href="#">{{ tag }}</a>
+        <NuxtLink to="#">{{ tag }}</NuxtLink>
       </div>
-      <a class="color-white" :href="link">
+      <NuxtLink class="color-white" :to="link">
         <h3 class="text-20-medium color-white mb-20">{{ title }}</h3>
-      </a>
+      </NuxtLink>
       <slot></slot>
-      <a class="cardLink btn btn-arrow-up" :href="link">
+      <NuxtLink class="cardLink btn btn-arrow-up" :to="link">
         <svg class="icon-16" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"></path>
         </svg>
-      </a>
+      </NuxtLink>
     </div>
   </div>
 </template>
